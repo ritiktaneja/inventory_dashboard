90fa355fb0cbb0e5ce3cb788f3067661d987eb88
Ritik Taneja <ritiktaneja10@gmail.com>
2020-05-17 03:16:54 +0530

    Added allotment page

---
+++ src/views/Allotment.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <div class="d-flex">
+        <h2 class=" grey--text"> Allotments </h2>
+        <v-btn small color="warning ml-auto"> <v-icon class="pr-1 ml-0 pl-0">restore</v-icon> Traceback</v-btn>
+    </div>
+       
+    <AllotmentTable/>
+
+
+  </v-container>
+
+</template>
+<script>
+import AllotmentTable from '../components/allotmentTable'
+export default {
+    components :{
+        AllotmentTable
+    }
+    
+    
+}
+</script>
